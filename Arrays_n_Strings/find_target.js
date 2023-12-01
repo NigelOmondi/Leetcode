97651a047463826a5e4df922f6e2c739a86798c4
NigelOmondi <nigelomondi62@gmail.com>
2023-12-01 22:02:03 +0300

    Find a pair that equals target sum

---
+++ Arrays_n_Strings/find_target.js
@@ -0,0 +1,22 @@
+// Works with a sorted array
+function findTargetedSum(arr, target) {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left < right) {
+        let currentSum = arr[left] + arr[right];
+        
+        if (currentSum == target) {
+            return true;
+        }
+        if (currentSum > target) {
+            right--;
+        } else {
+            left++;
+        }
+    
+    }
+    return false;
+}
+
+console.log(findTargetedSum([35, 42, 46, 73, 99], 88))
